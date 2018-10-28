b2edf97c8e1e7bc9c215ce70e11a44295551fe11
Tomas Cury <tomas.cury@eticasolucoes.com.br>
2018-10-28 17:01:40 -0300

    Integrate Consul

--- micronaut-sample/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookCatalogueClient.java
+++ micronaut-sample/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookCatalogueClient.java
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
 import io.reactivex.Flowable;
 
-@Client("http://localhost:8081")
+@Client(id = "bookcatalogue")
 public interface BookCatalogueClient extends BookCatalogueOperations {
 
     @Get("/books")
--- micronaut-sample/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookInventoryClient.java
+++ micronaut-sample/bookrecommendation/src/main/java/example/micronaut/bookrecommendation/BookInventoryClient.java
@@ -6,7 +6,7 @@ import io.reactivex.Maybe;
 
 import javax.validation.constraints.NotBlank;
 
-@Client("http://localhost:8082")
+@Client(id = "bookinventory")
 public interface BookInventoryClient extends BookInventoryOperations {
 
     @Get("/books/stock/{isbn}")
