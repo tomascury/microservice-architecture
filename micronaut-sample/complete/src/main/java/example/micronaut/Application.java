dc8e1554de20a97cbc33b0290443b2b8e4d6551f
Tomas Cury <tomas.cury@eticasolucoes.com.br>
2018-10-28 10:02:45 -0300

    Adding swagger

--- micronaut-sample/complete/src/main/java/example/micronaut/Application.java
+++ micronaut-sample/complete/src/main/java/example/micronaut/Application.java
@@ -1,7 +1,20 @@
 package example.micronaut;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hello World",
+                version = "0.0",
+                description = "My API",
+                license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+                contact = @Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
--- micronaut-sample/complete/src/main/java/example/micronaut/HelloController.java
+++ micronaut-sample/complete/src/main/java/example/micronaut/HelloController.java
@@ -3,14 +3,37 @@ package example.micronaut;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.validation.Validated;
+import io.reactivex.Single;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Controller("/hello")
+import javax.validation.constraints.NotBlank;
+
+@Controller("/")
+@Validated
 public class HelloController {
 
-    @Get("/")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String index() {
-        return "Hello World";
+    /**
+     * @param name The person's name
+     * @return The greeting
+     */
+    @Get(uri = "/hello/{name}", produces = MediaType.TEXT_PLAIN)
+    @Operation(summary = "Greets a person",
+            description = "A friendly greeting is returned"
+    )
+    @ApiResponse(
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type = "string"))
+    )
+    @ApiResponse(responseCode = "400", description = "Invalid Name Supplied")
+    @ApiResponse(responseCode = "404", description = "Person not found")
+    @Tag(name = "greeting")
+    public Single<String> hello(@Parameter(description = "The name of the person") @NotBlank String name) {
+        return Single.just("Hello " + name + "!");
     }
 }
